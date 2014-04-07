69fda2c6f0a76c488b99d1d361589840dbe507c1
Chhavi Nijhawan <cnijhawa@sjc-xdm-106.(none)>
2014-04-07 14:15:37 -0700

    Adding copyright info to crc_checker.py

--- crc_checker_n7k.py
+++ crc_checker_n7k.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2014 Cisco Systems Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
 #The following python script checks for CRC errors on all interfaces.
 #If crc error > threshold(10) on any interface,  then it prints that interface
 """
@@ -12,11 +29,10 @@ finished running CRC checker script
 ----------------------------------
 
 Switch # dir bootflash:scripts
-946    Feb 10 14:50:36 2014  crc.py
+Feb 10 14:50:36 2014  crc.py
 
 """
 
-#!/bin/env python
 from cisco import *
 import re
 import sys
